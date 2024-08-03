c426379209e283b9c7ae20d4251cd94552e6b4b6
jangjunha <hi@jangjunha.me>
2024-08-03 22:34:46 +0900

    feat: make Action Equatable

--- Sources/HeekeyCore/Reducer/Action.swift
+++ Sources/HeekeyCore/Reducer/Action.swift
@@ -1,10 +1,10 @@
-public enum Action {
+public enum Action: Equatable {
   case 완성문자(String)
   case 조합문자(조합문자)
   case 기능(기능)
 }
 
-public enum 조합문자 {
+public enum 조합문자: Equatable {
   case ㄱ
   case ㄴ
   case ㄹ
@@ -17,7 +17,7 @@ public enum 조합문자 {
   case ㅡ
 }
 
-public enum 기능 {
+public enum 기능: Equatable {
   case 획추가
   case 쌍자음
   case backspace
